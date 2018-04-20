8f19e7da993be2c22a592c24cc5e93717b421e93
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-04-19 20:15:37 -0400

    initial commit

---
+++ greeter.rb
@@ -0,0 +1,54 @@
+class MegaGreeter
+    attr_accessor :names
+
+    # create the object
+    def initialize(naems = "World")
+        @names = names
+    end
+
+    def say_hi
+        if @names.nil?
+            puts "..."
+        elsif @names.respond_to?("each")
+            # @names is a list that can be iterated
+            @names.each do |name|
+                puts "Hello #{name}!"
+            end
+        else
+            puts "Hello #{@names}!"
+        end
+    end
+
+    def say_bye
+        if @names.nil?
+            puts "..."
+        elsif @names.respond_to?("join")
+            # join the list elements with commas
+            puts "Goodbye #{@names.join(", ")}. Come back soon!"
+        else
+            puts "Goodbye #{@names}. Come back soon!"
+        end
+    end
+end
+
+if __FILE__ == $0
+    mg = MegaGreeter.new
+    mg.say_hi
+    mg.say_bye
+
+    # change name to "Pete"
+    mg.names = "Pete"
+    mg.say_hi
+    mg.say_bye
+
+    # change the name to an array of names
+    mg.names = ["George", "Frank", "Jane"]
+    mg.say_hi
+    mg.say_bye
+
+    # change to nil
+    mg.names = nil
+    mg.say_hi
+    mg.say_bye
+end
+        
